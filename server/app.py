36ba47ef229bb1b1a7d187c7a13312efd064858d
Victor <wanjala94vic@gmail.com>
2023-10-17 23:15:09 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, render_template
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from models import db, Zookeeper, Enclosure, Animal
 
@@ -9,9 +10,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
+
 
 @app.route('/')
 def home():
@@ -19,16 +21,19 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    return render_template('animal.html', animal=animal)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    return render_template('zookeeper.html', zookeeper=zookeeper)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.get(id)
+    return render_template('enclosure.html', enclosure=enclosure)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
